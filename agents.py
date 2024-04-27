16ab9fd112c56055c06e7e6ac30d8b2ed98efae2
mayankchugh-learning <mayankchugh.learning@gmail.com>
2024-04-27 12:54:11 +0800

    end-2-end code added

---
+++ agents.py
@@ -0,0 +1,81 @@
+from crewai import Agent
+from textwrap import dedent
+from langchain.llms import Ollama
+
+from tools.search_tools import SearchTools
+from tools.calculator_tools import CalculatorTools
+
+"""
+Creating Agents Cheat Sheet:
+- Think like a boss. Work backwards from the goal and think which employee 
+    you need to hire to get the job done.
+- Define the Captain of the crew who orient the other agents towards the goal. 
+- Define which experts the captain needs to communicate with and delegate tasks to.
+    Build a top down structure of the crew.
+
+Goal:
+- Create a 7-day travel itinerary with detailed per-day plans,
+    including budget, packing suggestions, and safety tips.
+
+Captain/Manager/Boss:
+- Expert Travel Agent
+
+Employees/Experts to hire:
+- City Selection Expert 
+- Local Tour Guide
+
+
+Notes:
+- Agents should be results driven and have a clear goal in mind
+- Role is their job title
+- Goals should actionable
+- Backstory should be their resume
+"""
+
+
+
+class TravelAgents:
+    def __init__(self): 
+        self.Ollama = Ollama(model="mistral")
+
+    def expert_travel_agent(self):
+        return Agent(
+            role="Expert Travel Agent",
+            backstory=dedent(
+                f"""Expert in travel planning and logistics. 
+                I have decades of expereince making travel iteneraries."""),
+            goal=dedent(f"""
+                        Create a 7-day travel itinerary with detailed per-day plans,
+                        include budget, packing suggestions, and safety tips.
+                        """),
+            tools=[
+                SearchTools.search_internet,
+                CalculatorTools.calculate
+            ],
+            verbose=True,
+            llm=self.Ollama,
+        )
+
+    def city_selection_expert(self):
+        return Agent(
+            role="City Selection Expert",
+            backstory=dedent(
+                f"""Expert at analyzing travel data to pick ideal destinations"""),
+            goal=dedent(
+                f"""Select the best cities based on weather, season, prices, and traveler interests"""),
+            tools=[SearchTools.search_internet],
+            verbose=True,
+            llm=self.Ollama,
+        )
+
+    def local_tour_guide(self):
+        return Agent(
+            role="Local Tour Guide",
+            backstory=dedent(f"""Knowledgeable local guide with extensive information
+        about the city, it's attractions and customs"""),
+            goal=dedent(
+                f"""Provide the BEST insights about the selected city"""),
+            tools=[SearchTools.search_internet],
+            verbose=True,
+            llm=self.Ollama,
+        )
